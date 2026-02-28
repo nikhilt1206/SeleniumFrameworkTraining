e73fe64b35070741221db168374c3b5b0ac9205c
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-02-28 09:52:35 +0530

    Daily Commit

--- Introduction/src/main/java/com/selenium/introduction/Locators.java
+++ Introduction/src/main/java/com/selenium/introduction/Locators.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class Locators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
 		//We will get an error because of the synchronization issue 
@@ -26,6 +26,23 @@ public class Locators {
 		//using CSS selector locator to find and print the error message in the UI
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		
+		//link text locator - the element must have anchor <a> tag
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		
+		//xpath creation for name text field
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Nikhil");
+		//xpath creating using indexing (use when we have less attributes with same value)
+		driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("nikhilt1206@gmail.com");
+		Thread.sleep(2000);
+		//to clear the input field
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
+		//CSS style indexing
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("sowji10@gmail.com");
+		
+		//Locating element using parent to child tags
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("9454730443");
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();  
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
 		
 	}
 
