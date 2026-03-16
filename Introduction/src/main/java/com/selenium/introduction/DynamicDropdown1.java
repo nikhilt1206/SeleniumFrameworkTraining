084204ed0a1b52ac95db43684a1ecd236e8fe619
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-16 21:27:13 +0530

    Dynamic dropdowns

---
+++ Introduction/src/main/java/com/selenium/introduction/DynamicDropdown1.java
@@ -0,0 +1,20 @@
+package com.selenium.introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropdown1 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(2000);
+        //PARENT-CHILD XPATH
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='MAA']"))
+                .click();
+    }
+}
