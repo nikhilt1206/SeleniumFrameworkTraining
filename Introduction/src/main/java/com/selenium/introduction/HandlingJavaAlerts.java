7ae943124753d9b4e398ef88a8eadb19d508339e
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-23 22:04:23 +0530

    Alerts updated

--- Introduction/src/main/java/com/selenium/introduction/HandlingJavaAlerts.java
+++ Introduction/src/main/java/com/selenium/introduction/HandlingJavaAlerts.java
@@ -1,9 +1,15 @@
 package com.selenium.introduction;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HandlingJavaAlerts {
     public static void main(String[] args){
@@ -12,20 +18,25 @@ public class HandlingJavaAlerts {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         WebElement alertTextField = driver.findElement(By.id("name"));
         alertTextField.sendKeys(text);
         WebElement alertBtn = driver.findElement(By.id("alertbtn"));
         alertBtn.click();
         //To get the text present on the alert
-        System.out.println(driver.switchTo().alert().getText());
+        //System.out.println(driver.switchTo().alert().getText());
         //To switch context from browser to alerts
-        driver.switchTo().alert().accept();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        System.out.println(alert.getText());
+        alert.accept();
 
         //Another kind of alert having positive and negative fields
         WebElement cnfBtn = driver.findElement(By.id("confirmbtn"));
         cnfBtn.click();
-        System.out.println(driver.switchTo().alert().getText());
-        driver.switchTo().alert().dismiss();
+        Alert alert2 = wait.until(ExpectedConditions.alertIsPresent());
+        //System.out.println(driver.switchTo().alert().getText());
+        System.out.println(alert2.getText());
+        alert2.dismiss();
     }
 }
