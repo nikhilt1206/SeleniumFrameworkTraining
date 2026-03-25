f54c2972af31c868d34976e73e58c1191689ef6d
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-25 08:21:13 +0530

    Ecommerce shoping cart automation

--- Introduction/src/main/java/com/exercise/selenium/Exercise13EcommerceShopping.java
+++ Introduction/src/main/java/com/exercise/selenium/Exercise13EcommerceShopping.java
@@ -10,18 +10,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Exercise13EcommerceShopping {
-    public static void main(){
+    public static void main(String[] args){
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
         driver.manage().window().maximize();
-        String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
+        String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot","Carrot"};
+        int countOfItemsNeeded=0;
         List <WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
         for(int i=0;i<products.size();i++){
-            String name = products.get(i).getText();
-            List itemsNeededList = Arrays.asList(itemsNeeded);
-
-            if(itemsNeededList.contains(name)){
-                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+            String[] name = products.get(i).getText().split("-");
+            String fomattedList = name[0].trim();
+            List<String> itemsNeededList = Arrays.asList(itemsNeeded);
+            if(itemsNeededList.contains(fomattedList)){
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                countOfItemsNeeded++;
+                if(countOfItemsNeeded==itemsNeeded.length){
+                    break;
+                }
             }
         }
 
