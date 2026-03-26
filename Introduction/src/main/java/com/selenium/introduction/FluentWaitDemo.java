60b3481e354205fd1ae8117b21010af5169fee79
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-26 21:40:19 +0530

    Fluent wait code added

---
+++ Introduction/src/main/java/com/selenium/introduction/FluentWaitDemo.java
@@ -0,0 +1,37 @@
+package com.selenium.introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class FluentWaitDemo {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.cssSelector("[id='start'] button")).click();
+        //fluent wait
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                WebElement output = driver.findElement(By.cssSelector("[id='finish'] h4"));
+                if(output.isDisplayed()){
+                    return output;
+                }
+                else{
+                    return null;
+                }
+            }});
+        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
+    }
+}
